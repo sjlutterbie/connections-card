39ee271e98e693af2973d4f0ecc5dbdc4c032af3
Simon Lutterbie <simon@ltrbe.com>
2024-12-31 16:29:45 -0500

    Styles cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -13,27 +13,29 @@ function App() {
   return (
     <>
       <div id="section-header">
-        <span className="logo-character">S</span>
-        <div className="divider" />
-        <span className="games">Games</span>
+        <div id="logo-character">S</div>
+        <div id="divider" />
+        <div id="games">Games</div>
       </div>
       <div id="section-title">
-        <span className="title">Happy Birthday!</span>
-        <span className="date">January 31, 2025</span>
+        <span id="title">Happy Birthday!</span>
+        <span id="date">January 31, 2025</span>
       </div>
       <div id="section-icons">
         <img src={lightBulbSvg} />
         <img src={chartSvg} />
         <img src={questionSvg} />
       </div>
+      <div id="section-instructions">Create four groups of four!</div>
       <div id="section-game">
-        <div id="section-instructions">Create four groups of four!</div>
-        <div id="section-found-sets">
-          {foundSets.map((set) => {
-            const { category } = set;
-            return <FoundSet {...set} key={category} />;
-          })}
-        </div>
+        {foundSets.length ? (
+          <div id="section-found-sets">
+            {foundSets.map((set) => {
+              const { category } = set;
+              return <FoundSet {...set} key={category} />;
+            })}
+          </div>
+        ) : null}
         <div id="section-tiles">
           {availableItems.map((tile) => {
             return <Tile key={tile} label={tile} />;
