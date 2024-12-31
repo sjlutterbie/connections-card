15146263b3e27424b9d4ec0ab8b981614b9f4073
Simon Lutterbie <simon@ltrbe.com>
2024-12-31 16:06:33 -0500

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,53 @@
 import './App.css';
-import { GameLayout } from './GameLayout';
-import { GameProvider } from './GameProvider';
-import { SnackbarProvider } from './SnackbarProvider';
 import lightBulbSvg from './assets/light_bulb.svg';
 import chartSvg from './assets/chart.svg';
 import questionSvg from './assets/question.svg';
+import { useGameContext } from './game.context';
+import { FoundSet } from './FoundSet';
+import { Tile } from './Tile';
+import { Mistakes } from './Mistakes';
+import { GameControls } from './GameControls';
 
 function App() {
+  const { availableItems, foundSets } = useGameContext();
   return (
-    <SnackbarProvider>
-      <GameProvider>
-        <div id="section-header">
-          <span className="logo-character">S</span>
-          <div className="divider" />
-          <span className="games">Games</span>
+    <>
+      <div id="section-header">
+        <span className="logo-character">S</span>
+        <div className="divider" />
+        <span className="games">Games</span>
+      </div>
+      <div id="section-title">
+        <span className="title">Happy Birthday!</span>
+        <span className="date">January 31, 2025</span>
+      </div>
+      <div id="section-icons">
+        <img src={lightBulbSvg} />
+        <img src={chartSvg} />
+        <img src={questionSvg} />
+      </div>
+      <div id="section-game">
+        <div id="section-instructions">Create four groups of four!</div>
+        <div id="section-found-sets">
+          {foundSets.map((set) => {
+            const { category } = set;
+            return <FoundSet {...set} key={category} />;
+          })}
         </div>
-        <div id="section-title">
-          <span className="title">Happy Birthday!</span>
-          <span className="date">January 31, 2025</span>
+        <div id="section-tiles">
+          {availableItems.map((tile) => {
+            return <Tile key={tile} label={tile} />;
+          })}
         </div>
-        <div id="section-icons">
-          <img src={lightBulbSvg} />
-          <img src={chartSvg} />
-          <img src={questionSvg} />
+        <div id="section-mistakes">
+          Mistakes Remaining:
+          <Mistakes />
         </div>
-        <div id="section-game">
-          <GameLayout />
+        <div id="section-controls">
+          <GameControls />
         </div>
-      </GameProvider>
-    </SnackbarProvider>
+      </div>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './main.css';
+import App from './App.tsx';
+import { SnackbarProvider } from './SnackbarProvider.tsx';
+import { GameProvider } from './GameProvider.tsx';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('app-root')!).render(
   <StrictMode>
-    <App />
+    <SnackbarProvider>
+      <GameProvider>
+        <App />
+      </GameProvider>
+    </SnackbarProvider>
   </StrictMode>,
-)
+);
